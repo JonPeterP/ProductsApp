6ee2efc23d9efdb5e18ddcac06f77d71ee83ff9f
JonPeterP <jon.pomperada@paymaya.com>
2026-02-12 11:15:01 +0800

    Initial Commit

--- ProductsApp/ViewController.swift
+++ ProductsApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
-
+    
 
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductsTableViewCell")
+        
+        return cell ?? UITableViewCell()
+        
+    }
+}
+
+
 
